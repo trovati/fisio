cd8b0dc6150dc460d8858f6325b9c7fcea26ea6d
Watson Trovati Matos <watson@MacBook-Air-de-Watson.local>
2021-11-26 22:06:17 -0300

    Teste Jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var jQuery: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  constructor() {}
+
+    ngOnInit() {
+        jQuery("#myselector").style="display: none;";
+    }
 }
